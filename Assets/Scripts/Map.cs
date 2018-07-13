4aeb359b3a41f9e151a548f068893b33b53966ef
WYQkulu <41142839+WYQkulu@users.noreply.github.com>
2018-07-13 20:09:53 +0800

    change

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -1,64 +1,76 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
-public class Map: MonoBehaviour {
-    private Ray ray;
-    private RaycastHit click;
-
-    private Light m_light1;
-    private Light m_light2;
-    private GameObject m_lightobj1;
-    private GameObject m_lightobj2;
-
-    private GameObject ball;
-    private Transform ballTransform;
-    private Vector3 dis;
-  
-    // Use this for initialization
-    void Start () {
-        m_lightobj1 = GameObject.Find("Light5a (1)");
-        m_light1 = m_lightobj1.GetComponent<Light>();
-        //m_lightmat1 = m_lightobj1.GetComponent<Material>();
-
-        m_lightobj2 = GameObject.Find("Light5a (2)");
-        m_light2 = m_lightobj2.GetComponent<Light>();
-        //m_lightmat2 = m_lightobj2.GetComponent<Material>();
-
-        //m_lightmat1.color = new Color(255f, 0f, 0f);
-        //m_lightmat2.color = new Color(0f, 255f, 0f);
-        
-        m_light1.enabled = true ;
-        m_light1.color = Color.red;
-        m_light2.enabled = false;
-
-        ball = GameObject.Find("Ball");
-        ballTransform = ball.GetComponent<Transform>();
-    }
-
-    // Update is called once per frame
-    void Update () {
-        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Physics.Raycast(ray, out click);
-        //Vector3 dis = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(10, 0, 10));
-        if (click.collider.tag == "Ball" && Input.GetMouseButton(0))
-        {
-            dis = new Vector3(click.point.x,2.095f,click.point.z);
-            ballTransform.position = dis;
-        }
-        if (m_light2.enabled == true)
-        {
-            m_light1.enabled = false;
-        }
-    }
-    void OnTriggerStay(Collider collider )
+namespace Valve.VR.InteractionSystem
+{
+    [RequireComponent(typeof(Interactable))]
+    public class Map : MonoBehaviour
     {
-        if (collider.gameObject.name == "Ball")
+        private Light m_light1;
+        private Light m_light2;
+        private GameObject m_lightobj1;
+        private GameObject m_lightobj2;
+
+        private GameObject ball;
+        private Transform ballTransform;
+        private Vector3 dis;
+        private Interactable ballinter;
+
+        private Vector3 oldPosition;
+        private Quaternion oldRotation;
+
+        private Hand.AttachmentFlags attachmentFlags = Hand.defaultAttachmentFlags & (~Hand.AttachmentFlags.SnapOnAttach) & (~Hand.AttachmentFlags.DetachOthers);
+
+
+        // Use this for initialization
+        void Start()
         {
-            //Debug.Log("!!!");
-            m_light1.enabled = false;
-            m_light2.enabled = true;
-            m_light2.color = Color.green;
+            m_lightobj1 = GameObject.Find("Light5a (1)");
+            m_light1 = m_lightobj1.GetComponent<Light>();
+            //m_lightmat1 = m_lightobj1.GetComponent<Material>();
+
+            m_lightobj2 = GameObject.Find("Light5a (2)");
+            m_light2 = m_lightobj2.GetComponent<Light>();
+            //m_lightmat2 = m_lightobj2.GetComponent<Material>();
+
+            //m_lightmat1.color = new Color(255f, 0f, 0f);
+            //m_lightmat2.color = new Color(0f, 255f, 0f);
+
+            m_light1.enabled = true;
+            m_light1.color = Color.red;
+            m_light2.enabled = false;
+
+            ball = GameObject.Find("Ball");
+            ballTransform = ball.GetComponent<Transform>();
+            oldPosition = ballTransform.position;
+            ballinter = ball.GetComponent<Interactable>();
+
+    }
+
+        // Use controller to control the ball
+        private void HandHoverUpdate(Hand hand)
+        {
+            if (hand.GetStandardInteractionButtonDown() || ((hand.controller != null) && hand.controller.GetPressDown(Valve.VR.EVRButtonId.k_EButton_Grip)))
+            {
+                if (hand.currentAttachedObject == ball)
+                {
+                    hand.HoverLock(ballinter);
+                    hand.AttachObject(ball, attachmentFlags);
+                }
+            }
+        }
+        void OnTriggerStay(Collider collider)
+        {
+            if (collider.gameObject.name == "Ball")
+            {
+                //Debug.Log("!!!");
+                m_light1.enabled = false;
+                m_light2.enabled = true;
+                m_light2.color = Color.green;
+            }
         }
     }
+
 }
--- Assets/Scripts/PullLever.cs
+++ Assets/Scripts/PullLever.cs
@@ -1,108 +1,113 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
-public class PullLever : MonoBehaviour
+namespace Valve.VR.InteractionSystem
+
 {
-    private Ray ray;
-    private RaycastHit click;
-
-    private Light m_light1;
-    private Light m_light2;
-    private GameObject m_lightobj1;
-    private GameObject m_lightobj2;
-    //private Material m_lightmat1;
-    //private Material m_lightmat2;
-
-    private Transform Levers_1;
-    private Transform Levers_2;
-   /* private int temp1=0;
-    private bool time1 = true; 
-    private int temp2=0;
-    private bool time2 = true;*/
-
-    // Use this for initialization
-    void Start()
+    [RequireComponent(typeof(Interactable))]
+    public class PullLever : MonoBehaviour
     {
-        m_lightobj1 = GameObject.Find("Light5a (1)");
-        m_light1 = m_lightobj1.GetComponent<Light>();
-        //m_lightmat1 = m_lightobj1.GetComponent<Material>();
+        private Light m_light1;
+        private Light m_light2;
+        private GameObject m_lightobj1;
+        private GameObject m_lightobj2;
+        //private Material m_lightmat1;
+        //private Material m_lightmat2;
 
-        m_lightobj2 = GameObject.Find("Light5a (2)");
-        m_light2 = m_lightobj2.GetComponent<Light>();
-        //m_lightmat2 = m_lightobj2.GetComponent<Material>();
+        private Transform Levers_1;
+        private Transform Levers_2;
+        private GameObject Leversobj_1;
+        private GameObject Leversobj_2;
 
-        //m_lightmat1.color = new Color(255f, 0f, 0f);
-        //m_lightmat2.color = new Color(0f, 255f, 0f);
-        m_light1.enabled = false;
-        m_light2.enabled = false;
+        //Change this project to Steam VR
+        private Vector3 oldPosition;
+        private Quaternion oldRotation;
 
-        Levers_1 = GameObject.FindGameObjectWithTag("Spindle_1").GetComponent<Transform>();
-        Levers_2 = GameObject.FindGameObjectWithTag("Spindle_2").GetComponent<Transform>();
-    }
+        private Interactable leverinter_1;
+        private Interactable leverinter_2;
 
-    // Update is called once per frame
-    void Update()
-    {
-        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Physics.Raycast(ray, out click);
-        if (click.collider.tag == "Lever_1" && Input.GetMouseButton(0))
+        private float attachTime;
+
+        private Hand.AttachmentFlags attachmentFlags = Hand.defaultAttachmentFlags & (~Hand.AttachmentFlags.SnapOnAttach) & (~Hand.AttachmentFlags.DetachOthers);
+
+
+        void Start()
         {
-            /*if ((temp1 <= 3) && (time1==true))
+            m_lightobj1 = GameObject.Find("Light5a (1)");
+            m_light1 = m_lightobj1.GetComponent<Light>();
+            //m_lightmat1 = m_lightobj1.GetComponent<Material>();
+
+            m_lightobj2 = GameObject.Find("Light5a (2)");
+            m_light2 = m_lightobj2.GetComponent<Light>();
+            //m_lightmat2 = m_lightobj2.GetComponent<Material>();
+
+            //m_lightmat1.color = new Color(255f, 0f, 0f);
+            //m_lightmat2.color = new Color(0f, 255f, 0f);
+            m_light1.enabled = false;
+            m_light2.enabled = false;
+
+            Levers_1 = GameObject.FindGameObjectWithTag("Spindle_1").GetComponent<Transform>();
+            Levers_2 = GameObject.FindGameObjectWithTag("Spindle_2").GetComponent<Transform>();
+            Leversobj_1 = GameObject.FindGameObjectWithTag("Lever_1");
+            Leversobj_2 = GameObject.FindGameObjectWithTag("Lever_2");
+
+            leverinter_1 = Leversobj_1.GetComponent<Interactable>();
+            leverinter_2 = Leversobj_2.GetComponent<Interactable>();
+        }
+
+        //Change this to VR
+        private void HandHoverUpdate(Hand hand)
+        {
+            if (hand.GetStandardInteractionButtonDown() || ((hand.controller != null) && hand.controller.GetPressDown(Valve.VR.EVRButtonId.k_EButton_Grip)))
             {
-                temp1 = temp1 + 1;
-                //Debug.Log(temp1);
-                Levers_1.Rotate(Vector3.left, 30f);
+                if (hand.currentAttachedObject == Leversobj_1)
+                {
+                    // Save our position/rotation so that we can restore it when we detach
+                    oldPosition = Levers_1.position;
+                    oldRotation = Levers_1.rotation;
+
+                    // Call this to continue receiving HandHoverUpdate messages,
+                    // and prevent the hand from hovering over anything else
+                    hand.HoverLock(leverinter_1);
+
+                    // Attach this object to the hand
+                    hand.AttachObject(Leversobj_1, attachmentFlags);
+                    Levers_1.Rotate(Vector3.right, -3f);
+                }
+                if (hand.currentAttachedObject == Leversobj_2)
+                {
+                    // Save our position/rotation so that we can restore it when we detach
+                    oldPosition = Levers_2.position;
+                    oldRotation = Levers_2.rotation;
+
+                    // Call this to continue receiving HandHoverUpdate messages,
+                    // and prevent the hand from hovering over anything else
+                    hand.HoverLock(leverinter_2);
+
+                    // Attach this object to the hand
+                    hand.AttachObject(Leversobj_2, attachmentFlags);
+                    Levers_2.Rotate(Vector3.right, -3f);
+                }
             }
-            if ((temp1 > 3) && (time1 == true))
+        }
+        void Update()
+        {
+            if (((Levers_1.rotation.x <= Quaternion.Euler(new Vector3(-55, 0, 0)).x)
+                && ((Levers_1.rotation.x >= Quaternion.Euler(new Vector3(-65, 0, 0)).x)))
+                && ((Levers_2.rotation.x <= Quaternion.Euler(new Vector3(-85, 0, 0)).x)
+                && ((Levers_2.rotation.x >= Quaternion.Euler(new Vector3(-95, 0, 0)).x))))
             {
-                Levers_1.Rotate(Vector3.left, -30f);
-                temp1 = temp1 + 1;
-                //Debug.Log(temp1);
+                m_light1.enabled = false;
+                m_light2.enabled = true;
+                m_light2.color = Color.green;
             }
-            if (temp1 == 11)
+            else
             {
-                temp1 = 0;
-            }*/
-            Levers_1.Rotate(Vector3.right, -3f);
-        }
-
-
-        if (click.collider.tag == "Lever_2" && Input.GetMouseButton(0))
-        {
-            /* //Debug.Log("???");
-             if ((temp2 <= 3) && (time2 == true))
-             {
-                 temp2 = temp2 + 1;
-                 //Debug.Log(temp2);
-                 Levers_2.Rotate(Vector3.left, 30f);
-             }
-             if ((temp2 > 3) && (time2 == true))
-             {
-                 Levers_2.Rotate(Vector3.left, -30f);
-                 temp2 = temp2 + 1;
-                 //Debug.Log(temp2);
-             }
-             if (temp2 == 11)
-             {
-                 temp2 = 0;
-             }*/
-            Levers_2.Rotate(Vector3.right, -3f);
-        }
-        if(((Levers_1.rotation.x<=Quaternion.Euler(new Vector3(-55,0,0)).x)
-            &&((Levers_1.rotation.x >= Quaternion.Euler(new Vector3(-65, 0, 0)).x)))
-            && ((Levers_2.rotation.x <= Quaternion.Euler(new Vector3(-85, 0, 0)).x)
-            && ((Levers_2.rotation.x >= Quaternion.Euler(new Vector3(-95, 0, 0)).x))))
-        {
-            m_light1.enabled = false ;
-            m_light2.enabled = true;
-            m_light2.color = Color.green;
-        }
-        else
-        {
-            m_light1.enabled = true;
-            m_light1.color = Color.red;
+                m_light1.enabled = true;
+                m_light1.color = Color.red;
+            }
         }
     }
 }
-
